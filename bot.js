041967e9261beea8b183227d1ac964a6c5f1c690
MujtabaMamoun <44872348+MujtabaMamoun@users.noreply.github.com>
2018-12-17 05:32:53 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("524210243425533955")
+let channel =     client.channels.get("524217301378007044")
 setInterval(function() {
 channel.send(`**xRq l3eb**`);
 }, 30)
